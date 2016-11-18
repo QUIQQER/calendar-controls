2c72dc30419f3b195751a5f4a120bc9384e51178
Henning Leutz <leutz@pcsg.de>
2016-11-18 14:29:05 +0100

    fix: css loaded for Scheduler

--- bin/Scheduler.js
+++ bin/Scheduler.js
@@ -4,7 +4,9 @@
  * @author www.pcsg.de (Henning Leutz)
  */
 define('package/quiqqer/calendar-controls/bin/Scheduler', [
-    URL_OPT_DIR + 'quiqqer/calendar-controls/bin/htmlxScheduler/dhtmlxscheduler.js'
+    URL_OPT_DIR + 'quiqqer/calendar-controls/bin/htmlxScheduler/dhtmlxscheduler.js',
+    'css!package/quiqqer/calendar-controls/bin/htmlxScheduler/dhtmlxscheduler.css',
+    'css!package/quiqqer/calendar-controls/bin/htmlxScheduler/dhtmlxscheduler_flat.css'
 ], function () {
     "use strict";
     return window.scheduler;
