e3bc4ded8ab4a5adf3377736696ed114ce90ba76
Henning Leutz <leutz@pcsg.de>
2016-12-13 15:17:47 +0100

    fix: locale für scheduler

--- bin/Scheduler.js
+++ bin/Scheduler.js
@@ -3,11 +3,33 @@
  *
  * @author www.pcsg.de (Henning Leutz)
  */
-define('package/quiqqer/calendar-controls/bin/Scheduler', [
+var needles = [
     URL_OPT_DIR + 'quiqqer/calendar-controls/bin/htmlxScheduler/dhtmlxscheduler.js',
     'css!package/quiqqer/calendar-controls/bin/htmlxScheduler/dhtmlxscheduler.css',
     'css!package/quiqqer/calendar-controls/bin/htmlxScheduler/dhtmlxscheduler_flat.css'
-], function () {
+];
+
+var localeFile = '';
+
+if ("QUIQQER_PROJECT" in window) {
+    localeFile = URL_OPT_DIR + 'quiqqer/calendar-controls/bin/' +
+                 'htmlxScheduler/locale/' +
+                 'locale_' + window.QUIQQER_PROJECT.lang + '.js';
+
+    needles.push(localeFile);
+}
+
+if ("USER" in window) {
+    localeFile = URL_OPT_DIR + 'quiqqer/calendar-controls/bin/' +
+                 'htmlxScheduler/locale/' +
+                 'locale_' + window.USER.lang + '.js';
+
+    if (!needles.contains(localeFile)) {
+        needles.push(localeFile);
+    }
+}
+
+define('package/quiqqer/calendar-controls/bin/Scheduler', needles, function () {
     "use strict";
     return window.scheduler;
 });
