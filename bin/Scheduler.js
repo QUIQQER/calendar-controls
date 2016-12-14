1a6f58c89e43071c15baef080ee1bbbf79012ac8
Henning Leutz <leutz@pcsg.de>
2016-12-14 15:18:15 +0100

    fix: locale loading

--- bin/Scheduler.js
+++ bin/Scheduler.js
@@ -9,10 +9,18 @@ define('dhtmlxscheduler', [
     URL_OPT_DIR + 'quiqqer/calendar-controls/bin/htmlxScheduler/dhtmlxscheduler.js',
     'css!package/quiqqer/calendar-controls/bin/htmlxScheduler/dhtmlxscheduler.css',
     'css!package/quiqqer/calendar-controls/bin/htmlxScheduler/dhtmlxscheduler_flat.css'
-]);
+], function () {
+    console.warn(window.scheduler);
+});
 
 // locale
-var localeNeedles = ['dhtmlxscheduler'];
+require.config({
+    paths: {
+        'dhtmlxschedulerLocale': URL_OPT_DIR + 'quiqqer/calendar-controls/bin/htmlxScheduler/locale'
+    }
+});
+
+var localeNeedles = [];
 var localeFile    = '';
 
 if ("QUIQQER_PROJECT" in window) {
@@ -20,7 +28,7 @@ if ("QUIQQER_PROJECT" in window) {
                  'htmlxScheduler/locale/' +
                  'locale_' + window.QUIQQER_PROJECT.lang + '.js';
 
-    localeNeedles.push(localeFile);
+    localeNeedles.push(window.QUIQQER_PROJECT.lang);
 }
 
 if ("USER" in window) {
@@ -28,16 +36,32 @@ if ("USER" in window) {
                  'htmlxScheduler/locale/' +
                  'locale_' + window.USER.lang + '.js';
 
-    if (!localeNeedles.contains(localeFile)) {
-        localeNeedles.push(localeFile);
+    if (!localeNeedles.contains(window.USER.lang)) {
+        localeNeedles.push(window.USER.lang);
     }
 }
 
-define('dhtmlxschedulerLocale', localeNeedles);
+var i, len, key, lang;
 
-define('package/quiqqer/calendar-controls/bin/Scheduler', [
-    'dhtmlxschedulerLocale'
-], function () {
+var dhtmlxscheduler = ['dhtmlxscheduler'];
+var shim            = {};
+
+for (i = 0, len = localeNeedles.length; i < len; i++) {
+    lang = localeNeedles[i];
+    key  = 'dhtmlxschedulerLocale/locale_' + lang;
+
+    shim[key] = {
+        deps: ['dhtmlxscheduler']
+    };
+
+    dhtmlxscheduler.push(key);
+}
+
+requirejs.config({
+    shim: shim
+});
+
+define('package/quiqqer/calendar-controls/bin/Scheduler', dhtmlxscheduler, function () {
     "use strict";
     return window.scheduler;
 });
